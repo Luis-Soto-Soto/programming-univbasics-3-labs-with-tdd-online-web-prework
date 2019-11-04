9206d426c1a3a748fcdb190fb78e624f6063988a
Luis A Soto <super.angel.20@hotmail.com>
2019-11-04 01:56:43 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
 first_number = 4
 second_number = 2
-sum = 6
-difference = 2 
-product = 8 
-quotent = 2
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotent = first_number / second_number
